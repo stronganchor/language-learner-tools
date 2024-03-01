86b9836a96621c2de5d12677d8aede0debf1aa75
stronganchor <77190959+stronganchor@users.noreply.github.com>
2024-03-01 12:51:00 +0300

    vers

--- language-learner-tools.php
+++ language-learner-tools.php
@@ -325,7 +325,7 @@ function ll_tools_flashcard_widget($atts) {
     ), $atts);
 	
 	// Set the version numbers for the CSS and JS files. (This needs to be incremented whenever the file changes)
-	$js_version = '1.8.9';
+	$js_version = '1.9.0';
 	$flashcard_css_version = '1.1.9';
 	
     wp_enqueue_style('ll-tools-flashcard-style', plugins_url('flashcard-style.css', __FILE__), array(), $flashcard_css_version);
