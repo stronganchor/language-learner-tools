a27662e11be8ec3f0b0517eb16810ca84fb9c4cc
Mike Mesenbring <mikemesenbring2@bitbucket.org>
2024-04-26 12:45:46 +0300

    reverting non-working code for handling scaling issues for word_audio

--- js/word-audio.js
+++ js/word-audio.js
@@ -1,41 +1,10 @@
 var activeAudioPlayers = [];
-var maxAudioPlayers = 500;
 
-function createAudioPlayer(wordElement) {
-    var audioUrl = wordElement.getAttribute('data-audio-url');
-    var audioPlayer = new Audio(audioUrl);
-    
-    audioPlayer.addEventListener('play', function() {
-        pauseAllAudioPlayers();
-    });
-    
-    audioPlayer.addEventListener('ended', function() {
-        destroyAudioPlayer(audioPlayer);
-    });
-    
-    activeAudioPlayers.push(audioPlayer);
-    
-    if (activeAudioPlayers.length > maxAudioPlayers) {
-        var oldestAudioPlayer = activeAudioPlayers.shift();
-        destroyAudioPlayer(oldestAudioPlayer);
-    }
-}
-
-function destroyAudioPlayer(audioPlayer) {
-    audioPlayer.pause();
-    audioPlayer.src = '';
-    audioPlayer.load();
-    var index = activeAudioPlayers.indexOf(audioPlayer);
-    if (index > -1) {
-        activeAudioPlayers.splice(index, 1);
-    }
-}
 // Use the plugin's directory URL to construct the paths for the SVG files
 var pluginDirUrl = ll_word_audio_data.plugin_dir_url;
 var play_icon = '<img src="' + pluginDirUrl + 'media/play-symbol.svg" width="10" height="10" alt="Play" data-no-lazy="1">';
 var stop_icon = '<img src="' + pluginDirUrl + 'media/stop-symbol.svg" width="9" height="9" alt="Stop" data-no-lazy="1">';
 
-// Rest of the JavaScript code...
 function ll_toggleAudio(audioId) {
     var audio = document.getElementById(audioId);
     var icon = document.getElementById(audioId + '_icon');
@@ -67,11 +36,6 @@ function pauseAllAudioPlayers() {
 document.addEventListener('DOMContentLoaded', function() {
     var wordElements = document.querySelectorAll('.ll-word-audio');
     
-    // Load the first 500 audio players
-    for (var i = 0; i < maxAudioPlayers && i < wordElements.length; i++) {
-        createAudioPlayer(wordElements[i]);
-    }
-    
     wordElements.forEach(function(wordElement) {
         wordElement.addEventListener('click', function() {
             var audioPlayer = activeAudioPlayers.find(function(player) {
@@ -85,8 +49,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 } else {
                     audioPlayer.pause();
                 }
-            } else {
-                createAudioPlayer(wordElement);
             }
         });
     });
