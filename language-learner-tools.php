c61d71f97c2881962ac9741effb1333f5f5b5bca
stronganchor <77190959+stronganchor@users.noreply.github.com>
2024-12-06 11:19:07 +0300

    Update language-learner-tools.php

--- language-learner-tools.php
+++ language-learner-tools.php
@@ -125,4 +125,12 @@ function ll_tools_load_textdomain() {
 }
 add_action('plugins_loaded', 'll_tools_load_textdomain');
 
+function ll_tools_enqueue_confetti_script() {
+    // Print the canvas-confetti script tag in the header
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js" integrity="sha256-ZJ7Rw03+QdUh2juYIVeLnXqYIvDfOcofR/jMr16qPhA=" crossorigin="anonymous"></script>
+    <?php
+}
+add_action('wp_head', 'll_tools_enqueue_confetti_script');
+
 ?>
