2986079fd708043a4e9e4b43694d78ca8d82326c
stronganchor <stronganchortech@gmail.com>
2025-01-08 10:30:10 +0300

    Refactor image copyright grid shortcode for improved readability and add CSS enqueueing for the shortcode

--- image-copyright-grid-shortcode.php
+++ image-copyright-grid-shortcode.php
@@ -12,31 +12,34 @@
  */
 function ll_image_copyright_grid_shortcode($atts) {
     // Parse shortcode attributes; default posts_per_page = 12
-    $atts = shortcode_atts(array(
-        'posts_per_page' => 12, // How many images per page
-    ), $atts, 'image_copyright_grid');
+    $atts = shortcode_atts(
+        array(
+            'posts_per_page' => 12, // How many images per page
+        ),
+        $atts,
+        'image_copyright_grid'
+    );
 
     // Figure out the current page for pagination
-    // (Using 'paged' query var is the most common approach in WP)
-    $paged = get_query_var('paged') ? (int)get_query_var('paged') : 1;
+    $paged = get_query_var('paged') ? (int) get_query_var('paged') : 1;
 
     // Query only Word Images that have a non-empty copyright_info
     $args = array(
         'post_type'      => 'word_images',
         'post_status'    => 'publish',
         'paged'          => $paged,
-        'posts_per_page' => (int)$atts['posts_per_page'],
+        'posts_per_page' => (int) $atts['posts_per_page'],
         'meta_query'     => array(
             array(
                 'key'     => 'copyright_info',
-                'value'   => '',      // Compare with empty string
-                'compare' => '!=',    // "!=" means we only want rows where key != ''
+                'value'   => '',
+                'compare' => '!=', // means key != ''
             ),
         ),
     );
     $query = new WP_Query($args);
 
-    // Begin output buffering so we can return the final HTML
+    // Start output buffering
     ob_start();
 
     if ($query->have_posts()) {
@@ -49,7 +52,6 @@ function ll_image_copyright_grid_shortcode($atts) {
 
             echo '<div class="ll-image-copyright-grid-item">';
                 // Show the featured image at a small size
-                // e.g., "thumbnail", or use array(150, 150) for exact dimensions
                 if (has_post_thumbnail()) {
                     echo '<div class="ll-image-wrapper">';
                         the_post_thumbnail(array(150, 150)); // or 'thumbnail'
@@ -58,7 +60,7 @@ function ll_image_copyright_grid_shortcode($atts) {
                     echo '<div class="ll-image-wrapper no-image">No Image</div>';
                 }
 
-                // Title (optional)
+                // Optional title
                 echo '<h4 class="ll-word-image-title">' . get_the_title() . '</h4>';
 
                 // Copyright info
@@ -87,12 +89,26 @@ function ll_image_copyright_grid_shortcode($atts) {
         echo '<p>No Word Images found with copyright info.</p>';
     }
 
-    // Reset query so other loops/pages aren’t affected
+    // Reset query
     wp_reset_postdata();
 
-    // Return our buffered content
+    // Return buffered content
     return ob_get_clean();
 }
 add_shortcode('image_copyright_grid', 'll_image_copyright_grid_shortcode');
 
 
+/**
+ * Enqueues the CSS for the image copyright grid if the [image_copyright_grid] shortcode is used.
+ */
+function ll_maybe_enqueue_image_copyright_grid_styles() {
+    // Only enqueue if we're on a singular page/post with the shortcode present
+    if (is_singular()) {
+        $post = get_post();
+        if ($post && has_shortcode($post->post_content, 'image_copyright_grid')) {
+            // Use your custom function that sets the version by filemtime
+            ll_enqueue_asset_by_timestamp('/css/image-copyright-style.css', 'll-image-copyright-style');
+        }
+    }
+}
+add_action('wp_enqueue_scripts', 'll_maybe_enqueue_image_copyright_grid_styles');
