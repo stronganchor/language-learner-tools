038c05ad31b754b2472199ae79e81f596207c026
stronganchor <stronganchortech@gmail.com>
2026-02-23 11:30:25 +0300

    5.3.9

--- language-learner-tools.php
+++ language-learner-tools.php
@@ -3,7 +3,7 @@
 Plugin Name: Language Learner Tools
 Plugin URI: https://github.com/stronganchor/language-learner-tools
 Description: WordPress tools for building language-learning vocabulary content with word management, audio/image uploads, and ready-to-use flashcard quizzes and embeddable practice pages.
-Version: 5.3.8
+Version: 5.3.9
 Author: Strong Anchor Tech
 Author URI: https://stronganchortech.com
 Text Domain: ll-tools-text-domain
